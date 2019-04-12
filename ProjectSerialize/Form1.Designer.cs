34a12954c2cc18993fbd88961f621dcaa8cb2290
Ben Fisher <benstanfish@hotmail.com>
2019-04-12 05:21:58 -0700

    Commit

--- ProjectSerialize/Form1.Designer.cs
+++ ProjectSerialize/Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.ButtonSerialize = new System.Windows.Forms.Button();
             this.ButtonOpenFile = new System.Windows.Forms.Button();
-            this.dgv = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // ButtonSerialize
@@ -52,26 +52,26 @@
             this.ButtonOpenFile.Text = "Open File";
             this.ButtonOpenFile.UseVisualStyleBackColor = true;
             // 
-            // dgv
+            // dataGridView1
             // 
-            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(13, 13);
-            this.dgv.Name = "dgv";
-            this.dgv.Size = new System.Drawing.Size(600, 425);
-            this.dgv.TabIndex = 2;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(600, 425);
+            this.dataGridView1.TabIndex = 2;
             // 
             // FormSerialize
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.ButtonOpenFile);
             this.Controls.Add(this.ButtonSerialize);
             this.Name = "FormSerialize";
             this.ShowIcon = false;
             this.Text = "Project Serialize";
-            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -80,7 +80,7 @@
 
         private System.Windows.Forms.Button ButtonSerialize;
         private System.Windows.Forms.Button ButtonOpenFile;
-        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- ProjectSerialize/Program.cs
+++ ProjectSerialize/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+
 
 namespace ProjectSerialize
 {
@@ -18,21 +20,36 @@ namespace ProjectSerialize
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormSerialize());
 
+        }
 
 
+        public static void InitializeDataTable()
+        {
+            DataTable table = new DataTable("ParentTable");
+            DataColumn column;
+            DataRow row;
 
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "ID";
+            column.ReadOnly = true;
+            column.Unique = true;
 
+            table.Columns.Add(column);
 
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ParentItem";
+            column.AutoIncrement = false;
+            column.Caption = "ParentItem";
+            column.ReadOnly = false;
+            column.Unique = false;
 
-
+            table.Columns.Add(column);
 
 
 
         }
-
-
-
-
-
     }
+
 }
